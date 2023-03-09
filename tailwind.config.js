a6d9fe95889b1d73b8a25ba291cb8414fec1ee8f
halimdirgantara <halimdirgantara@gmail.com>
2023-03-09 13:11:09 +0700

    feat: tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,5 +20,10 @@ module.exports = {
         },
     },
 
-    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+    plugins: [
+        require('@tailwindcss/forms')({
+            strategy: 'class',
+        }),,
+        require('@tailwindcss/typography')
+    ],
 };
