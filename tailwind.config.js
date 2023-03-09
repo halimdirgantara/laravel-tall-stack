0ce50b88f4e1c11d91a9179c0abe756958158c16
halimdirgantara <halimdirgantara@gmail.com>
2023-03-09 13:48:53 +0700

    feat: user list using powergrid

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
     plugins: [
         require('@tailwindcss/forms')({
             strategy: 'class',
-        }),,
+        }),
         require('@tailwindcss/typography')
     ],
 };
